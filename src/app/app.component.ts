06260b59964787f0fa3e216074478e53f3788f2e
Milan Filipovic <milan.filipovic@rokolabs.com>
2023-10-16 12:07:55 +0200

    End of section 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title: string = 'Dating app';
-  users: any;
 
-  constructor(private http: HttpClient){
+  constructor(private accountService: AccountService){
 
   }
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users')
+    this.setCurrentUser();
   }
 
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
 
 }
