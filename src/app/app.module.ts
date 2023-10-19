0ddb8b7029f5689bffe01ce0fe34e73ab9d16362
Milan Filipovic <milan.filipovic@rokolabs.com>
2023-10-20 00:32:20 +0200

    End of section 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './components/members/member-list/member-list.component';
+import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../models/user';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +14,9 @@ export class NavComponent implements OnInit{
 
   model: any = {}
   currentUser$: Observable<User | null> = of(null);
-  constructor(public accountService: AccountService){
+  constructor(public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService){
 
   }
   ngOnInit(): void {
@@ -20,16 +24,17 @@ export class NavComponent implements OnInit{
   }
 
   login(){
-    console.log(this.model);
     this.accountService.login(this.model).subscribe({
-      next : response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+      next : () => this.router.navigateByUrl('/members'),
+      error: error =>{
+        console.log(error);
+        this.toastr.error(error.error);
+      }
     });
   }
 
   logout(){
     this.accountService.logut();
+    this.router.navigateByUrl('/');
   }
 }
