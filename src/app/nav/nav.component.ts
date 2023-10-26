b8918895c97fd974354a32bbd6010e25d2b12588
Milan Filipovic <milan.filipovic@rokolabs.com>
2023-10-26 13:49:05 +0200

    End of section 7

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -28,7 +28,7 @@ export class NavComponent implements OnInit{
       next : () => this.router.navigateByUrl('/members'),
       error: error =>{
         console.log(error);
-        this.toastr.error(error.error);
+        //this.toastr.error(error.error); ovo ti je sad u interceptor
       }
     });
   }
